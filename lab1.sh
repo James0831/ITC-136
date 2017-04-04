641057850af4421d284e8acc8dbadcb8e85c125f
James0831 <jdcarroll08@gmail.com>
2017-04-04 15:14:35 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -5,3 +5,8 @@ whoami    # Tells me whcih user I'm logged in as (WHO)
 ls        # Tells me what files are in the directory (WHAT)
 cd        # Allows me to change to other directory cd .. goes back a file(NAVIGATION)
 date      # What time is it on the machine (WHEN)
+
+/var/root cat test.sh                                                         
+#!/bin/sh                                                                       
+                                                                                
+echo "this is text... from the command. yay."
